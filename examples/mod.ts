de585008f2daada89d6314a186b8f4ff2d5d8cd0
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-16 19:51:30 +0200

    restructure examples

---
+++ examples/mod.ts
@@ -0,0 +1,18 @@
+export { csv } from "../examples/csv.ts";
+export {
+  booleanAttributes,
+  commentNode,
+  fragments,
+  html,
+  Kind, serializeFragments,
+  serializeNode,
+  shadowRoot,
+  textNode, type MCommentNode,
+  type MElement,
+  type MFragment,
+  type MNode,
+  type MSpacesAndComments,
+  type MTextNode,
+  type SerializationOptions
+} from "../examples/html.ts";
+export { expr } from "./arithmetic-expressions.ts";
