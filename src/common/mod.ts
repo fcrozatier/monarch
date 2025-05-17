3b296e0fac2a094e0dfae2ad113ac1f161a8e833
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-17 15:03:34 +0200

    use plural in common modules

--- src/common/mod.ts
+++ src/common/mod.ts
@@ -1,5 +1,5 @@
-export * from "./list.ts";
-export * from "./regex.ts";
+export * from "./lists.ts";
+export * from "./regexes.ts";
 export * from "./tokens.ts";
 export * from "./whitespaces.ts";
 export * from "./numbers.ts";
