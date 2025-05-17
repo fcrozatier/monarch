edb27acfaecb088471a101447c058b2e09f63f7e
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-17 14:53:16 +0200

    rename recursion folder

--- src/combinators/mod.ts
+++ src/combinators/mod.ts
@@ -1,4 +1,4 @@
 export * from "./alternation/mod.ts";
 export * from "./iteration/mod.ts";
-export * from "./lazy-evaluation/mod.ts";
+export * from "./recursion/mod.ts";
 export * from "./sequencing/mod.ts";
