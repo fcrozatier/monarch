1ea96408a1866c423d8cac631285ebccb40776ab
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-17 19:04:03 +0200

    describe main module

--- mod.ts
+++ mod.ts
@@ -1,7 +1,20 @@
 /**
  * Build parsers by composing structural combinators and chaining semantic refinements
  *
- * Combinators are composed with function composition
+ * Combinators describe your grammar with function composition:
+ *
+ * - alternation
+ * - iteration
+ * - recursion
+ * - reduction
+ * - sequencing
+ *
+ * Refinements express semantic actions with method chaining:
+ *
+ * - customizing error messages
+ * - filtering
+ * - mapping
+ * - skipping
  *
  * Refinements are expressed with method chaining
  *
