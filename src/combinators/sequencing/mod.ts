ddb721848b97a145b6953c748937c03b25c694a2
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-17 14:59:50 +0200

    create a reduction group

--- src/combinators/sequencing/mod.ts
+++ src/combinators/sequencing/mod.ts
@@ -5,8 +5,4 @@
  */
 
 export { between } from "./between.ts";
-export { foldL } from "./foldL.ts";
-export { foldL1 } from "./foldL1.ts";
-export { foldR } from "./foldR.ts";
-export { foldR1 } from "./foldR1.ts";
 export { seq } from "./seq.ts";
