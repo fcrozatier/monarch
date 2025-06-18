7805ccced185d632ad1557224af55de1955f8fa7
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-18 11:58:25 +0200

    add links

--- mod.ts
+++ mod.ts
@@ -3,18 +3,20 @@
  *
  * Combinators describe your grammar with function composition:
  *
- * - alternation
- * - iteration
- * - recursion
- * - reduction
- * - sequencing
+ * - [alternation](https://jsr.io/@fcrozatier/monarch/doc/~/alt)
+ * - [iteration](https://jsr.io/@fcrozatier/monarch/doc/~/many)
+ * - [recursion](https://jsr.io/@fcrozatier/monarch/doc/~/lazy)
+ * - [reduction](https://jsr.io/@fcrozatier/monarch/doc/~/foldL)
+ * - [sequencing](https://jsr.io/@fcrozatier/monarch/doc/~/seq)
  *
  * Refinements express semantic actions with method chaining:
  *
- * - customizing error messages
- * - filtering
- * - mapping
- * - skipping
+ * - [customizing error messages](https://jsr.io/@fcrozatier/monarch/doc/~/Parser.prototype.error)
+ * - [filtering](https://jsr.io/@fcrozatier/monarch/doc/~/Parser.prototype.filter)
+ * - [mapping](https://jsr.io/@fcrozatier/monarch/doc/~/Parser.prototype.map)
+ * - [skipping](https://jsr.io/@fcrozatier/monarch/doc/~/Parser.prototype.skipTrailing)
+ *
+ * For a introductory walkthrough of the various combinators, see the following [guide](https://github.com/fcrozatier/monarch?tab=readme-ov-file#getting-started-guide)
  *
  * @module
  */
